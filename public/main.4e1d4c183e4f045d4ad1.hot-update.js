bcf7aa5641bce98d22ce00c5276254cb0e3667d5
ColinMassard <colinmassard@gmail.com>
2023-07-11 18:06:29 +0200

    UP(Home) font update

---
+++ public/main.4e1d4c183e4f045d4ad1.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdateboilerplate_prismic"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("777af309c9c647d024f1")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi40ZTFkNGMxODNlNGYwNDVkNGFkMS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ib2lsZXJwbGF0ZS1wcmlzbWljL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJzb3VyY2VzQ29udGVudCI6WyJfX3dlYnBhY2tfcmVxdWlyZV9fLmggPSAoKSA9PiAoXCI3NzdhZjMwOWM5YzY0N2QwMjRmMVwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
